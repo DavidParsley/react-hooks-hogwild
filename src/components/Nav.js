26a55ba09e4e959291d3888a402b5ce272654be0
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-03 21:09:22 +0300

    adds a parent

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,28 @@
 import React from "react";
+import HogCreator from "./HogCreator";
 import piggy from "../assets/porco.png";
 
-const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
-	);
-};
+export default function Nav({ onAddHog, setSortingCriteria }) {
+  return (
+    <div className="navWrapper">
+      <span className="headerText">HogWild</span>
+      <div className="TwirlyPig">
+        <img src={piggy} className="App-logo" alt="piggy" />
+      </div>
+      <span className="normalText">A React App for County Fair Hog Fans</span>
+      <br />
+      <br />
+      <HogCreator onAddHog={onAddHog} />
+      <div className="sortByWrapper">
+        <label>
+          Sort By:
+          <select onChange={(e) => setSortingCriteria(e.target.value)}>
+            <option value="name">Name</option>
+            <option value="weight">Weight</option>
+          </select>
+        </label>
+      </div>
+    </div>
+  );
+}
 
-export default Nav;
